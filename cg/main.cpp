ffc13a02af8d68583a8c096307e0894a0b983238
Андрей Михин <mikhinandrei@MacBook-Air-Andrej.local>
2018-12-02 13:59:49 +0300

    Initial Commit

---
+++ cg/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cg
+//
+//  Created by Андрей Михин on 02.12.18.
+//  Copyright © 2018 Андрей Михин. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
